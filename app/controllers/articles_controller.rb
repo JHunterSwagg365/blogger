3046c6d86d453a68be7da5976a5396efd399f8cc
Addler Pierre-Louis <jhunterswagg365@gmail.com>
2013-07-18 11:22:10 -0400

    Basic Blogger completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,41 @@ class ArticlesController < ApplicationController
 	def show
 	@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.create(article_params)
+		redirect_to article_path(@article)
+
+		flash.notice = "Article '#{@article.title}' has been created!"
+	end
+	
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+
+		flash.notice = "Article '#{@article.title}' has been destroyed successfully!"
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+	  @article = Article.find(params[:id])
+	  @article.update(article_params)
+
+	  flash.notice = "Article '#{@article.title}' updated!"
+
+	  redirect_to article_path(@article)
+	end
+
+	def article_params
+		params.require("article").permit("title", "body")
+	end
+
 end
